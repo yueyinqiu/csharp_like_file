bb705e7c5bb78f8396f6998b98e538885c8e2632
yueyinqiu <yueyinqiu@outlook.com>
2024-09-21 23:41:45 +0800

    4

--- csharp_like_file/csdir/__init__.pyi
+++ csharp_like_file/csdir/__init__.pyi
@@ -2,7 +2,7 @@ import os as _os
 import pathlib as _pathlib
 
 
-def create_directory(path: str | _os.PathLike,
+def create_directory(path: str | _os.PathLike[str],
                      unix_create_mode: int = 0o777) -> _pathlib.Path:
     result = _pathlib.Path(path)
     result.mkdir(mode=unix_create_mode, parents=True, exist_ok=True)
--- csharp_like_file/csfile/__init__.pyi
+++ csharp_like_file/csfile/__init__.pyi
@@ -2,60 +2,60 @@ import os as _os
 import typing as _typing
 
 
-def append_all_lines(path: str | _os.PathLike,
+def append_all_lines(path: str | _os.PathLike[str],
                      contents: _typing.Iterable[str],
                      encoding: str = "utf8") -> None:
     with open(path, "a", encoding=encoding) as file:
         file.writelines(contents)
 
 
-def append_all_text(path: str | _os.PathLike,
+def append_all_text(path: str | _os.PathLike[str],
                     contents: str,
                     encoding: str = "utf8") -> None:
     with open(path, "a", encoding=encoding) as file:
         file.write(contents)
 
 
-def copy(source_file_name: str | _os.PathLike,
+def copy(source_file_name: str | _os.PathLike[str],
          dest_file_name: str) -> None:
     import shutil
     shutil.copyfile(source_file_name, dest_file_name)
 
 
-def delete(path: str | _os.PathLike) -> None:
+def delete(path: str | _os.PathLike[str]) -> None:
     _os.remove(path)
 
 
-def exists(path: str | _os.PathLike | None) -> bool:
+def exists(path: str | _os.PathLike[str] | None) -> bool:
     if path is None:
         return False
     return _os.path.exists(path)
 
 
-def move(source_file_name: str | _os.PathLike,
-         dest_file_name: str | _os.PathLike) -> None:
+def move(source_file_name: str | _os.PathLike[str],
+         dest_file_name: str | _os.PathLike[str]) -> None:
     import shutil
     shutil.move(source_file_name, dest_file_name)
 
 
-def read_all_bytes(path: str | _os.PathLike) -> bytes:
+def read_all_bytes(path: str | _os.PathLike[str]) -> bytes:
     with open(path, "rb") as file:
         return file.read()
 
 
-def read_all_lines(path: str | _os.PathLike,
+def read_all_lines(path: str | _os.PathLike[str],
                    encoding: str = "utf8") -> list[str]:
     with open(path, "r", encoding=encoding) as file:
         return file.readlines()
 
 
-def read_all_text(path: str | _os.PathLike,
+def read_all_text(path: str | _os.PathLike[str],
                   encoding: str = "utf8") -> str:
     with open(path, "r", encoding=encoding) as file:
         return file.read()
 
 
-def read_lines(path: str | _os.PathLike,
+def read_lines(path: str | _os.PathLike[str],
                encoding: str = "utf8") -> _typing.Iterable[str]:
     with open(path, "r", encoding=encoding) as file:
         for line in file.readline():
@@ -64,20 +64,20 @@ def read_lines(path: str | _os.PathLike,
             yield line
 
 
-def write_all_bytes(path: str | _os.PathLike,
+def write_all_bytes(path: str | _os.PathLike[str],
                     bytes_: bytes) -> None:
     with open(path, "wb") as file:
         file.write(bytes_)
 
 
-def write_all_lines(path: str | _os.PathLike,
+def write_all_lines(path: str | _os.PathLike[str],
                     contents: _typing.Iterable[str],
                     encoding: str = "utf8") -> None:
     with open(path, "w", encoding=encoding) as file:
         file.writelines(contents)
 
 
-def write_all_text(path: str | _os.PathLike,
+def write_all_text(path: str | _os.PathLike[str],
                    contents: str,
                    encoding: str = "utf8") -> None:
     with open(path, "w", encoding=encoding) as file:
